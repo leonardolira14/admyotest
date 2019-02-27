528fd1f3e084ad339e07e5407d0eb13802a0f7e6
leonardolira14 <leon_lira_@hotmail.com>
2019-02-27 10:51:57 -0600

    first committ

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/admyo/';
-
+//$config['base_url'] = 'http://localhost/admyo/';
+$config['base_url'] = 'https://test.admyo.com/admyotest/';
 /*
 |--------------------------------------------------------------------------
 | Index File
